fe5d1467846d13ab1c169e531efdfb12b2c86a35
softwareguycoder <softwareguycoder@gmail.com>
2018-07-30 12:41:43 -0400

    30 Jul 2018 Removed symbols from deleted char_to_long function

--- console_core/include/symbols.h
+++ console_core/include/symbols.h
@@ -9,9 +9,3 @@
 #define EXACTLY_CORRECT			1
 #define NO_INPUT				-506
 #define TOO_LONG				-507
-
-#define INVALID_CHARS			-304
-#define BUFFER_UNDERFLOW		-305
-#define BUFFER_OVERFLOW			-306
-#define INVALID_BASE			-307
-#define UNSPECIFIED_ERROR		-308
