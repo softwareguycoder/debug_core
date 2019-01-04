f3d732cd9068967f11159d456373e6bcdfd65465
softwareguycoder <softwareguycoder@gmail.com>
2019-01-04 09:36:27 -0500

    4 Jan 2019 Added documentation to log_info function

--- debug_core/include/debug_core.h
+++ debug_core/include/debug_core.h
@@ -8,6 +8,12 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+/**
+ * @name log_info
+ * @details Logs a formatted [INFO] message to standard output.
+ * @param message The message to be displayed, with any printf-compatible format specifiers that may be applicable.
+ * @param vargs Format arguments.
+ */
 void log_info(const char* message, ...);
 void log_warning(const char* message, ...);
 void log_error(const char* message, ...);
