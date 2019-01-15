399c5fec0da978468f28a47cc36a3170ea040ab4
softwareguycoder <softwareguycoder@gmail.com>
2019-01-15 14:15:04 -0500

    15 Jan 2019 Added documentation to the project and updated the comments in the debug_core.h file

--- debug_core/include/debug_core.h
+++ debug_core/include/debug_core.h
@@ -5,34 +5,61 @@
  *      Author: bhart
  */
 
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
+#ifndef __DEBUG_CORE_H__
+#define __DEBUG_CORE_H__
 
+/**
+ * @name DEBUG_MESSAGE_PREFIX
+ * @brief Constant that defines the string prefix for a message at the DEBUG level.
+ */
 #define DEBUG_MESSAGE_PREFIX	"[   DEBUG   ]"
+
+/**
+ * @name ERROR_MESSAGE_PREFIX
+ * @brief Constant that defines the string prefix for a message at the ERROR level.
+ */
 #define ERROR_MESSAGE_PREFIX	"[   ERROR   ]"
+
+/**
+ * @name WARN_MESSAGE_PREFIX
+ * @brief Constant that defines the string prefix for a message at the WARN level.
+ */
 #define WARN_MESSAGE_PREFIX		"[   WARN    ]"
+
+/**
+ * @name INFO_MESSAGE_PREFIX
+ * @brief Constant that defines the string prefix for a message at the INFO level.
+ */
 #define INFO_MESSAGE_PREFIX     "[   INFO    ]"
 
 /**
- * Exit code to be returned from processes upon successful completion; operating system/standard
+ * @name OK
+ * @brief Exit code to be returned from processes upon successful completion; operating system/standard
  * library functions also (generally, as a rule) return this to indicate success.
  */
+#ifndef OK
 #define OK		0		// Code to return to the operating system to indicate successful program termination
+#endif //OK
 
 /**
- * Exit code to be returned from processes when an error condition has arisen; some operating system/standard
+ * @name ERROR
+ * @brief Exit code to be returned from processes when an error condition has arisen; some operating system/standard
  * library functions also return this upon failure, although generally, as a rule, these functions return some
  * other nonzero value.
  */
+#ifndef ERROR
 #define ERROR	-1		// Code to return to the operating system to indicate an error condition
+#endif //ERROR
 
 /**
- * Data type to encode a one-or-zero, true-or-false value.
+ * @name BOOL
+ * @brief Data type to encode a one-or-zero, true-or-false value.
  */
 typedef enum { FALSE, TRUE } BOOL;
 
 /**
- * Turns this library's messages on or off.
+ * @name toggle_debug
+ * @brief Turns this library's messages on or off.
  * @param enabled TRUE to enable this library; FALSE to turn all messages off.
  * @remarks This library is enabled by default.
  */
@@ -40,13 +67,34 @@ void toggle_debug(BOOL enabled);
 
 /**
  * @name log_info
- * @details Logs a formatted [INFO] message to standard output.
- * @param message The message to be displayed, with any printf-compatible format specifiers that may be applicable.
- * @param vargs Format arguments.
+ * @brief Logs a formatted [INFO] message to standard output.
+ * @param message The message to be displayed, with any printf-compatible format specifiers.
+ * @param ... Format arguments.
  */
 void log_info(const char* message, ...);
+
+/**
+ * @name log_warning
+ * @brief Logs a formatted [WARN] message to standard output.
+ * @param message The message to be displayed, with any printf-compatible format specifiers.
+ * @param ... Format arguments.
+ */
 void log_warning(const char* message, ...);
+
+/**
+ * @name log_error
+ * @brief Logs a formatted [ERROR] message to standard output.
+ * @param message The message to be displayed, with any printf-compatible format specifiers.
+ * @param ... Format arguments.
+ */
 void log_error(const char* message, ...);
+
+/**
+ * @name log_debug
+ * @brief Logs a formatted [DEBUG] message to standard output.
+ * @param message The message to be displayed, with any printf-compatible format specifiers.
+ * @param ... Format arguments.
+ */
 void log_debug(const char* message, ...);
 
-#endif /* __DEBUG_H__ */
+#endif //__DEBUG_CORE_H__
