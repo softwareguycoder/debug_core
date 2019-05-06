7ee96c18db6ccf3f6207e6e2f1e570241a1e2dfc
softwareguycoder <softwareguycoder@gmail.com>
2019-05-06 13:42:22 -0400

    Breaking the defn of the BOOL type into api_core library

--- debug_core/include/stdafx.h
+++ debug_core/include/stdafx.h
@@ -9,6 +9,7 @@
 #include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 #include <pthread.h>
 
+#include <../../../api_core/api_core/include/api_core.h>
 #include <../../../common_core/common_core/include/common_core.h>
 
 #endif //__DEBUG_CORE_STDAFX_H__
